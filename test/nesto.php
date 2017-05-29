d04a045f5e30398a9056d82fa7984fb52962536b
emrah_okic <emrahokic0@gmail.com>
2017-05-29 18:36:11 +0200

    k

--- test/nesto.php
+++ test/nesto.php
@@ -2,7 +2,7 @@
 $to = "emrah.okic@edu.fit.ba";
 $subject = "My subject";
 $txt = "Hello world!";
-$headers = "From: emrahokic96@hotmail.com" . "\r\n";
+$headers = "From: contact@spottland.com" . "\r\n";
 
 mail($to,$subject,$txt,$headers);
 ?>
