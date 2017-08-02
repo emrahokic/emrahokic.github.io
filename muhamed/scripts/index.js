1e0a44ae5fbb685610007437440d8793dca61247
emrah_okic <emrahokic0@gmail.com>
2017-08-02 13:41:45 +0200

    f

---
+++ muhamed/scripts/index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://raw.githubusercontent.com/emrahokic/Web1/master/data.json' // the path to the animation json
+})
